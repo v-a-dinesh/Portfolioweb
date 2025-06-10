e63b5a1c4240dd40159b8c6fe2ccd2fae84520b9
V A DINESH <dineshva2004@gmail.com>
2025-06-10 10:28:43 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,11 +1,11 @@
 export const Bio = {
   name: "DINESH V A",
   roles: [
-    "Associate Software Engineer Intern",
+    "Software Engineer",
     "Full Stack Developer",
     "Python Developer",
     "SDE Intern",
-    "AI/ML Enthusiast",
+    "AI/ML Research Intern",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
